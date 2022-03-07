3845439b3d17334c0b759b8b790415162e125631
Nika <nikakvrivishvili777@gmail.com>
2022-03-07 19:42:48 +0400

    fixed errors commit

--- src/user/models.py
+++ src/user/models.py
@@ -7,14 +7,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
 
-    # first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    # last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    # region = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    # school = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    # school_class = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    # email = db.Column(db.String(255, collation='NOCASE'), nullable=False, unique=True)
-
-    username = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    region = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    school = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    school_class = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    email = db.Column(db.String(255, collation='NOCASE'), nullable=False, unique=True)
 
     email_confirmed_at = db.Column(db.DateTime())
     password = db.Column(db.String(255), nullable=False, server_default='')
@@ -22,6 +20,16 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary='user_roles', lazy=True)
 
     answers = db.relationship('Answer', backref='users', lazy=True)
+
+    def __init__(self, first_name, last_name, region, school, school_class, email, email_confirmed_at, password):
+        self.email = email
+        self.school_class = school_class
+        self.school = school
+        self.region = region
+        self.last_name = last_name
+        self.first_name = first_name
+        self.email_confirmed_at = email_confirmed_at
+        self.password = password
 
 
 class Role(db.Model):
