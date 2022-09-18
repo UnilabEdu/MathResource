a2a8358179ee09813c3bb1593b943b7ff87fda43
MxKavt <mariam.kavtaradze.10@iliauni.edu.ge>
2022-09-18 18:14:09 +0400

    admin panel design, user list, user task details and etc

--- src/admin/__init__.py
+++ src/admin/__init__.py
@@ -1,5 +1,5 @@
 from flask import redirect, url_for, request
-from flask_admin import Admin, BaseView, expose
+from flask_admin import Admin, BaseView, expose, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.contrib.fileadmin import FileAdmin
 from flask_admin.menu import MenuLink
@@ -8,19 +8,26 @@ from src.user.models import User
 from src.questions.models import UsersTasks
 from markupsafe import Markup
 from flask_admin.helpers import get_form_data
+from flask_admin.form.fields import DateTimeField
 
 from src.extensions import db
 from src.user.models import User
 
 
-class AuthMixin(object):
+class AuthMixin(ModelView):
 
     def is_accessible(self):
-        return current_user.has_roles('Admin')
+        if current_user.is_authenticated:
+            return current_user.has_roles('Admin')
+        else:
+            return False
 
     def _handle_view(self, name, **kwargs):
         if not self.is_accessible():
             return redirect(url_for("auth.login"))
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login', next=request.url))
 
 
 class LogOutLink(MenuLink):
@@ -28,11 +35,22 @@ class LogOutLink(MenuLink):
         return url_for("auth.logout")
 
 
-class AdminModelView(ModelView):
+class AdminView(AdminIndexView):
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            return current_user.has_roles('Admin')
+        else:
+            return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login', next=request.url))
+
+
+class AdminModelView(AuthMixin):
     pass
 
 
-class TeacherModelView(ModelView, AuthMixin):
+class TeacherModelView(AuthMixin):
     can_create = False
     can_delete = False
     can_edit = True
@@ -43,13 +61,14 @@ class TeacherModelView(ModelView, AuthMixin):
     can_export = True
 
 
-class UserView(ModelView, AuthMixin):
+class UserView(AuthMixin):
     can_create = True
     can_delete = False
     column_list = ("first_name", "last_name", "region", "school", "school_class", "email", "email_confirmed_at", 'Tasks')
     column_exclude_list = ['password', ]
     column_searchable_list = ['first_name', 'last_name', 'email']
     can_export = True
+    form_overrides = dict(email_confirmed_at=DateTimeField)
 
     def use_button(view, context, model, name):
         checkout_url = url_for('.checkout_view')
@@ -58,7 +77,7 @@ class UserView(ModelView, AuthMixin):
                     <form action="{checkout_url}" method="POST">
                         <input id="user_id" name="user_id"  type="hidden" value="{user_id}">
                         <button type='submit'>Checkout</button>
-                    </form
+                    </form>
                 '''.format(checkout_url=checkout_url, user_id=model.id)
 
         return Markup(_html)
@@ -76,7 +95,7 @@ class UserView(ModelView, AuthMixin):
 
         return self.render('admin/result.html', user_tasks = user_tasks)
 
-class RoleView(ModelView, AuthMixin):
+class RoleView(AuthMixin):
     can_create = False
     can_delete = False
     can_edit = True
@@ -86,11 +105,11 @@ class RoleView(ModelView, AuthMixin):
     can_export = True
 
 
-class StaticView(FileAdmin, AuthMixin):
+class StaticView(FileAdmin):
     can_export = True
 
 
-class UsersTasksView(ModelView, AuthMixin):
+class UsersTasksView(AuthMixin):
     can_create = False
     can_delete = False
     can_edit = True
@@ -99,7 +118,7 @@ class UsersTasksView(ModelView, AuthMixin):
     can_export = True
 
 
-class UserRolesView(ModelView, AuthMixin):
+class UserRolesView(AuthMixin):
     can_create = False
     can_delete = True
     can_edit = True
@@ -110,28 +129,28 @@ class UserRolesView(ModelView, AuthMixin):
     can_export = True
 
 
-class ContactsView(ModelView, AuthMixin):
+class ContactsView(AuthMixin):
     can_create = True
     can_delete = True
     can_edit = True
     column_list = ('contact_source', 'info')
 
 
-class AboutView(ModelView, AuthMixin):
+class AboutView(AuthMixin):
     can_create = True
     can_delete = True
     can_edit = True
     column_list = ('info',)
 
 
-class TeamView(ModelView, AuthMixin):
+class TeamView(AuthMixin):
     can_create = True
     can_edit = True
     column_filters = ['position']
     column_list = ('person', 'position', 'description', 'linkedin', 'github', 'dribble', 'behance')
 
 
-class DocumentsView(ModelView, AuthMixin):
+class DocumentsView(AuthMixin):
     can_edit = True
     can_delete = True
     can_create = True
@@ -146,4 +165,5 @@ class TestView(BaseView):
         return self.render('admin/test_html.html', users=users)
 
 
-admin = Admin(name="Admin Panel", template_mode='bootstrap4')
+admin = Admin(name="Admin Panel", template_mode='bootstrap4', index_view=AdminView())
+
--- src/extensions.py
+++ src/extensions.py
@@ -3,9 +3,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_admin import Admin
 
-
 db = SQLAlchemy()
 login_manager = LoginManager()
-admin = Admin()
 migrate = Migrate()
 
