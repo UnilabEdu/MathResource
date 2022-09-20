088e4504f17f5ebb94e903e1cbbcfda90a2ac0f4
Mariam Kavtaradze <97850258+MxKavt@users.noreply.github.com>
2022-09-20 15:19:15 +0400

    added some extra funcs (#22)
    * admin panel edits: crud for contacts, about, terms and conditions, team. edited few typos here and there
    
    * added task progress
    
    * admin panel design, user list, user task details and etc
    
    * added some extra funcs

---
+++ src/admin/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request
+from src.user.models import User
+
+admins_blueprint = Blueprint('admins',
+                             __name__,
+                             template_folder='templates/admin')
+
+
+@admins_blueprint.route('/')
+def admin():
+    return render_template("admin/index.html")
+
+
+@admins_blueprint.route('/task_progress')
+def task_progress():
+    return render_template("admin/task_progress.html")
+
+
+@admins_blueprint.route('/user_stats', methods=['POST', 'GET'])
+def user_stats():
+    user_id = request.form['get_id']
+    user = User.query.get(user_id)
+    return render_template('admin/task_progress.html', user=user)
--- src/config.py
+++ src/config.py
@@ -18,3 +18,18 @@ class TestConfig(object):
     USER_EMAIL_SENDER_EMAIL = "noreply@example.com"
     USER_ENABLE_CONFIRM_EMAIL = False
 
+    # ReCaptcha Settings
+    RECAPTCHA_USE_SSL = False
+    RECAPTCHA_OPTIONS = {'theme': 'white'}
+    RECAPTCHA_PUBLIC_KEY = '6Lco4wsiAAAAAFI7WNBQ4730aj50b70ZwpThCVms'
+    RECAPTCHA_PRIVATE_KEY = '6Lco4wsiAAAAAC1q79bhCBXqeVoGUJ21Uuh8ngqJ'
+
+    # Flask_Mail Settings
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USERNAME = '63230bc4e9e304'
+    MAIL_PASSWORD = 'ccd0d346245a48'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    SECURITY_PASSWORD_SALT = 'my_precious_two'
